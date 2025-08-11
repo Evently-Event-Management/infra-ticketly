47d88de0d09f5fa5aad5fd93e8814e65aecc5735
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-08-11 11:55:49 +0530

    Enhance CORS configuration and update server port in application settings

--- api-gateway/src/main/java/com/ticketly/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/ticketly/apigateway/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.ticketly.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -14,15 +15,16 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
+                .cors(Customizer.withDefaults())
                 .authorizeExchange(exchange -> exchange
                         // All routes passing through the gateway must be authenticated.
+                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyExchange().authenticated()
                 )
                 // Configure the gateway as a Resource Server to validate incoming JWTs.
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 // Disable CSRF as we are using a stateless token-based approach.
                 .csrf(ServerHttpSecurity.CsrfSpec::disable);
-
         return http.build();
     }
 }
