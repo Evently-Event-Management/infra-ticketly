f676ee4fc25dfcc78f682b0cfbb96a80fb5c61a6
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-08-31 03:21:05 +0530

    Update .gitignore and add Terraform lock file; enhance outputs for SQS queues

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -7,10 +7,22 @@ output "ticketly_dev_user_secret_key" {
   sensitive = true
 }
 
-output "keycloak_db_endpoint" {
+output "ticketly_db_endpoint" {
   value = aws_db_instance.ticketly_db.endpoint
 }
 
 output "sqs_session_on_sale_url" {
   value = aws_sqs_queue.session_on_sale.url
+}
+
+output "sqs_session_on_sale_arn" {
+  value = aws_sqs_queue.session_on_sale.arn
+}
+
+output "sqs_session_closed_url" {
+  value = aws_sqs_queue.session_closed.url
+}
+
+output "sqs_session_closed_arn" {
+  value = aws_sqs_queue.session_closed.arn
 }
