330d7a6cb83c8582254dc0c930fd7481432c4e3c
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-08-31 14:28:22 +0530

    Add output for EC2 public IP in Terraform configuration

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -11,6 +11,10 @@ output "ticketly_db_endpoint" {
   value = aws_db_instance.ticketly_db.endpoint
 }
 
+output "ec2_ip" {
+  value = aws_instance.ticketly_ec2.public_ip
+}
+
 output "sqs_session_on_sale_url" {
   value = aws_sqs_queue.session_on_sale.url
 }
