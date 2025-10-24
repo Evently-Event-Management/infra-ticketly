b905e25ab65821649194b6c60be66215b7e92ac4
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-10-24 05:54:27 +0530

    Add Web Application Firewall (WAF) configuration for ALB

--- aws/prod/load_balancer.tf
+++ aws/prod/load_balancer.tf
@@ -71,3 +71,98 @@ resource "aws_lb_target_group_attachment" "worker" {
   target_id        = each.value.id
   port             = 80
 }
+
+# -----------------------------------------------------------------------------
+# Web Application Firewall (WAF) - protects the ALB against common exploits
+# -----------------------------------------------------------------------------
+
+resource "aws_wafv2_web_acl" "alb" {
+  name  = "ticketly-alb-waf-${terraform.workspace}"
+  scope = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "ticketlyAlbWaf"
+    sampled_requests_enabled   = true
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesCommonRuleSet"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "commonRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+    priority = 2
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "knownBadInputsRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesSQLiRuleSet"
+    priority = 3
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesSQLiRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "sqliRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  tags = {
+    Name        = "ticketly-alb-waf"
+    Environment = terraform.workspace
+  }
+}
+
+resource "aws_wafv2_web_acl_association" "alb" {
+  resource_arn = aws_lb.cluster.arn
+  web_acl_arn  = aws_wafv2_web_acl.alb.arn
+}
